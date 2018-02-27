339c39ef2a51ef0dd41a8336543b6baaa0bf63df
Max Cliff <maxcliff@Maxs-MBP.fios-router.home>
2018-02-26 23:04:19 -0500

    final

--- assets/Gifs.js
+++ assets/Gifs.js
@@ -9,13 +9,14 @@ var topics=["soccer", "football", "basketball", "lacrosse", "volleyball", "gymna
 
 
     function sports(){
+        $("#buttons-div").empty()
         for(var i =0; i < topics.length; i++){
             var newBtn = $("<button>")
             newBtn.html(topics[i])
             newBtn.addClass("topics")
             newBtn.attr("data-attr", topics[i])
             $("#buttons-div").append(newBtn)
-
+            
         }
     }
         sports()
@@ -30,13 +31,44 @@ var topics=["soccer", "football", "basketball", "lacrosse", "volleyball", "gymna
                 method: "GET" 
             }).done(function(response){
                 console.log(response)
+                $("#gifs-div").empty();
                 for(var i =0; i < response.data.length; i++){
                     console.log(response.data)
                     var newGif= $("<img>")
-                    newGif.attr("src", response.data[i].images.original.url)
+                    newGif.attr("data-still", response.data[i].images.original_still.url)
+                    newGif.attr("data-animate", response.data[i].images.original.url)
+                    newGif.attr("src", response.data[i].images.original_still.url)
+                    newGif.attr(response.data[i].rating)
                     newGif.css("max-width", "200px")
+                    newGif.addClass("gif-image")
                     $("#gifs-div").append(newGif)
+                    
+                    
 
                 }
             })
+   
+        })
+
+        $("#btnSearch").on("click", function(){
+           var userInput= $("#mySearch").val()
+           topics.push(userInput)
+
+            sports()
+        })
+
+        var animated = false;
+        $(document).on("click", ".gif-image", function(){
+            if (!animated){
+                var animation = $(this).attr("data-animate")
+                $(this).attr("src", animation)
+                animated = true;
+                console.log(animated)
+                
+            }else {
+                var still= $(this).attr("data-still")
+                $(this).attr("src", still)
+                animated = false;
+                console.log(animated)
+            }
         })
